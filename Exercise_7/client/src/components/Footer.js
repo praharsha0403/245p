c6623e8b4bcebfa947e03711781a6a780c2a37ba
praharsha0403 <pnelatur@uci.edu>
2025-11-13 09:36:34 +0530

    Add files via upload

---
+++ Exercise_7/client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p>© 2025 Company Name. All rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Exercise_7/client/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <a href="#home">Dashboard</a>
+      <a href="#services">Services</a>
+      <a href="#clients">Clients</a>
+      <a href="#contact">Contact</a>
+    </div>
+  );
+}
+
+export default Sidebar;
