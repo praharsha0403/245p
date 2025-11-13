39ee559d798fcf5c8392cddadeb246214eca0d11
praharsha0403 <pnelatur@uci.edu>
2025-11-13 09:40:40 +0530

    Add files via upload

---
+++ Exercise_7/client/src/components/MainContent.js
@@ -0,0 +1,10 @@
+function MainContent() {
+  return (
+    <div style={{ marginLeft: "200px", padding: "20px", marginTop: "80px" }}>
+      <h1>Welcome to the Main Content</h1>
+      <p>This is the main area of the application.</p>
+    </div>
+  );
+}
+
+export default MainContent;
